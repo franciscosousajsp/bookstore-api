b1760033f412010ccedf7c53c26a993d709b3fed
franciscosousajsp <franciscosousajsp@gmail.com>
2022-09-11 23:41:12 -0300

    projeto inicio

---
+++ src/test/java/com/francisco/bookstore/BookstoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.francisco.bookstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookstoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
