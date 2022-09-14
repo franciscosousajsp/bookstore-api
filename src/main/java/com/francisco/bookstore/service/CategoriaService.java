1d8c3fc11d709c672a397b5fc16a3dc0530725b6
franciscosousajsp <franciscosousajsp@gmail.com>
2022-09-13 23:07:41 -0300

    tratamento de erros

--- src/main/java/com/francisco/bookstore/service/CategoriaService.java
+++ src/main/java/com/francisco/bookstore/service/CategoriaService.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 
 import com.francisco.bookstore.domian.Categoria;
 import com.francisco.bookstore.repositories.CategoriaRepository;
+import com.francisco.bookstore.service.exceptions.ObjectNotFoundException;
 
 @Service
 public class CategoriaService {
@@ -17,6 +18,6 @@ public class CategoriaService {
 	public Categoria findById(Integer id) {
 		
 		Optional<Categoria> obj = repository.findById(id);	
-		return obj.orElse(null);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado! Id " + id + ", Tipo "+Categoria.class.getName()));
 	}
 }
