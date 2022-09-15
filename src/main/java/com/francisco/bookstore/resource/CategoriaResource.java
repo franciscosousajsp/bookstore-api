c5473453b68df70c32b1a31608a6a484b45c21fa
franciscosousajsp <franciscosousajsp@gmail.com>
2022-09-14 23:44:19 -0300

    tratamento de erros

--- src/main/java/com/francisco/bookstore/resource/CategoriaResource.java
+++ src/main/java/com/francisco/bookstore/resource/CategoriaResource.java
@@ -2,13 +2,12 @@ package com.francisco.bookstore.resource;
 
 import java.net.URI;
 import java.util.List;
-import java.util.stream.Collector;
 import java.util.stream.Collectors;
 
-import javax.servlet.ServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -28,36 +27,41 @@ public class CategoriaResource {
 
 	@Autowired
 	private CategoriaService service;
-	
+
 	@GetMapping(value = "/{id}")
-	public ResponseEntity<Categoria> findById(@PathVariable Integer id){
-		
-		Categoria obj = service.findById(id);	
+	public ResponseEntity<Categoria> findById(@PathVariable Integer id) {
+
+		Categoria obj = service.findById(id);
 		return ResponseEntity.ok().body(obj);
 	}
-	
+
 	@GetMapping
-	public ResponseEntity<List<CategoriaDTO>> findAll(){
-		
+	public ResponseEntity<List<CategoriaDTO>> findAll() {
+
 		List<Categoria> list = service.findAll();
 		List<CategoriaDTO> listDTO = list.stream().map(obj -> new CategoriaDTO(obj)).collect(Collectors.toList());
-		return ResponseEntity.ok().body(listDTO);	
+		return ResponseEntity.ok().body(listDTO);
 	}
-	
-	
+
 	@PostMapping
-	public ResponseEntity<Categoria> create(@RequestBody Categoria obj){
+	public ResponseEntity<Categoria> create(@RequestBody Categoria obj) {
 		obj = service.create(obj);
 		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
 		return ResponseEntity.created(uri).build();
-		
+
 	}
-	
+
 	@PutMapping(value = "/{id}")
-	public ResponseEntity<CategoriaDTO> update(@PathVariable Integer id, @RequestBody CategoriaDTO objDTO){
+	public ResponseEntity<CategoriaDTO> update(@PathVariable Integer id, @RequestBody CategoriaDTO objDTO) {
 		Categoria newObj = service.update(id, objDTO);
 		return ResponseEntity.ok().body(new CategoriaDTO(newObj));
-		
+
 	}
-	
-} 
+
+	@DeleteMapping(value = "/{id}")
+	public ResponseEntity<Void> delete(@PathVariable Integer id) {
+		service.delete(id);
+		return ResponseEntity.noContent().build();
+	}
+
+}
--- src/main/java/com/francisco/bookstore/service/CategoriaService.java
+++ src/main/java/com/francisco/bookstore/service/CategoriaService.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import com.francisco.bookstore.domian.Categoria;
@@ -16,31 +17,45 @@ public class CategoriaService {
 
 	@Autowired
 	private CategoriaRepository repository;
-	
+
 	public Categoria findById(Integer id) {
-		
-		Optional<Categoria> obj = repository.findById(id);	
-		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado! Id " + id + ", Tipo "+Categoria.class.getName()));
+
+		Optional<Categoria> obj = repository.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id " + id + ", Tipo " + Categoria.class.getName()));
 	}
-	
-	public List<Categoria> findAll(){
-		
+
+	public List<Categoria> findAll() {
+
 		return repository.findAll();
 	}
-	
+
 	public Categoria create(Categoria obj) {
 		obj.setId(null);
 		return repository.save(obj);
-		
+
 	}
 
 	public Categoria update(Integer id, CategoriaDTO objDTO) {
-		
+
 		Categoria obj = findById(id);
 		obj.setNome(objDTO.getNome());
 		obj.setDescricao(objDTO.getDescricao());
-		
+
 		return repository.save(obj);
 	}
-	
+
+	public void delete(Integer id) {
+		findById(id);
+
+		try {
+			repository.deleteById(id);	
+			
+		} catch (DataIntegrityViolationException e) {
+			throw new com.francisco.bookstore.service.exceptions.
+			DataIntegrityViolationException("Categoria não pode ser deletada! possuem livros associados");
+		}
+
+	}
+
 }
