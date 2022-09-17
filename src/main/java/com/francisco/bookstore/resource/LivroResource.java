20ed6030b89b2abd86b2db1916b4bd24923f03c6
franciscosousajsp <franciscosousajsp@gmail.com>
2022-09-17 16:02:46 -0300

    post do livro

--- src/main/java/com/francisco/bookstore/resource/LivroResource.java
+++ src/main/java/com/francisco/bookstore/resource/LivroResource.java
@@ -1,21 +1,26 @@
 package com.francisco.bookstore.resource;
 
+import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.francisco.bookstore.domian.Livro;
 import com.francisco.bookstore.dtos.LivroDTO;
 import com.francisco.bookstore.service.LivroService;
+
 
 @RestController
 @RequestMapping(value = "/livros")
@@ -23,30 +28,48 @@ public class LivroResource {
 
 	@Autowired
 	private LivroService service;
-	
-	
+
 	@GetMapping(value = "/{id}")
-	public ResponseEntity<Livro> findById(@PathVariable Integer id){
+	public ResponseEntity<Livro> findById(@PathVariable Integer id) {
+
 		Livro obj = service.findById(id);
-		
 		return ResponseEntity.ok().body(obj);
 	}
-	
+
 	@GetMapping
-	public ResponseEntity<List<LivroDTO>> findAll(@RequestParam(value = "categoria", defaultValue = "0") Integer id_cat){
+	public ResponseEntity<List<LivroDTO>> findAll(
+			@RequestParam(value = "categoria", defaultValue = "0") Integer id_cat) {
+
 		List<Livro> list = service.findAll(id_cat);
 		List<LivroDTO> listDTO = list.stream().map(obj -> new LivroDTO(obj)).collect(Collectors.toList());
-		
+
 		return ResponseEntity.ok().body(listDTO);
 	}
-	
+
 	@PutMapping(value = "/{id}")
-	public ResponseEntity<Livro> update(@PathVariable Integer id, @RequestBody Livro obj){
-		
-		Livro newObj = service.update(id, obj);	
+	public ResponseEntity<Livro> update(@PathVariable Integer id, @RequestBody Livro obj) {
+
+		Livro newObj = service.update(id, obj);
 		return ResponseEntity.ok().body(newObj);
-		
+
 	}
-	
-	
+
+	@PatchMapping(value = "/{id}")
+	public ResponseEntity<Livro> updatePatc(@PathVariable Integer id, @RequestBody Livro obj) {
+
+		Livro newObj = service.update(id, obj);
+		return ResponseEntity.ok().body(newObj);
+
+	}
+
+	@PostMapping
+	public ResponseEntity<Livro> create(@RequestParam(value = "categoria", defaultValue = "0") Integer id_cat,
+			@RequestBody Livro obj) {
+
+		Livro newObj = service.create(id_cat, obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentContextPath().path("/livros/{id}").buildAndExpand(newObj.getId()).toUri();
+		
+		return ResponseEntity.created(uri).build();
+
+	}
 }
--- src/main/java/com/francisco/bookstore/service/LivroService.java
+++ src/main/java/com/francisco/bookstore/service/LivroService.java
@@ -6,7 +6,7 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-
+import com.francisco.bookstore.domian.Categoria;
 import com.francisco.bookstore.domian.Livro;
 import com.francisco.bookstore.repositories.LivroRepository;
 import com.francisco.bookstore.service.exceptions.ObjectNotFoundException;
@@ -47,6 +47,16 @@ public class LivroService {
 		newObj.setNome_autor(obj.getNome_autor());
 		newObj.setTexto(obj.getTexto());
 		
+	}
+
+
+	public Livro create(Integer id_cat, Livro obj) {
+		
+		obj.setId(null);
+		Categoria cat = categoriaService.findById(id_cat);
+		obj.setCategoria(cat);
+		
+		return repository.save(obj);
 	}
 	
 	
